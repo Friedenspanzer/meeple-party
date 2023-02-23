0d5ca7f82e7436a52e58709b97ffd2c870780222
Pascal Simon <pascaltobiassimon@gmail.com>
2023-02-23 13:52:13 +0100

    Add simple data synchronization for BGG data

---
+++ utility/games.ts
@@ -0,0 +1,155 @@
+import { Game, GameExtended } from "@/datatypes/game";
+import { XMLParser } from "fast-xml-parser";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const DEV = process.env.NODE_ENV === "development";
+
+const MONTHLY = 30 * 24 * 60 * 60;
+const DAILY = 60 * 60 * 24;
+
+export type GameIds = number | number[];
+
+export async function fetchGames(gameIds: GameIds): Promise<Game[]> {
+  const ids = Array.isArray(gameIds) ? gameIds : [gameIds];
+
+  const gamesFromDatabase = await prisma.game.findMany({
+    where: {
+      id: { in: ids },
+    },
+  });
+  const staleIds = gamesFromDatabase
+    .filter(
+      (g) =>
+        !g.updatedAt ||
+        (Date.now().valueOf() - g.updatedAt.valueOf()) / 1000 > MONTHLY
+    )
+    .map((g) => g.id);
+
+  const missingIds = ids.filter(
+    (id) => !gamesFromDatabase.find((g) => g.id === id)
+  );
+
+  //TODO Update fetchGameData to fetch more than one game at once
+  const missingGames = (await Promise.all(missingIds.map(fetchGameData))).map(
+    convertForDataBase
+  );
+  const staleGames = (await Promise.all(staleIds.map(fetchGameData))).map(
+    convertForDataBase
+  );
+
+  if (!!missingGames && missingGames.length > 0) {
+    if (DEV) {
+      console.log(
+        "Inserting missing games",
+        missingGames.map((g) => `${[g.id]} ${g.name}}`)
+      );
+    }
+    await prisma.game.createMany({ data: missingGames });
+  }
+  if (!!staleGames && staleGames.length > 0) {
+    if (DEV) {
+      console.log(
+        "Updating stale games",
+        staleGames.map((g) => `${[g.id]} ${g.name}}`)
+      );
+    }
+    for (let staleGame of staleGames) {
+      await prisma.game.update({
+        data: staleGame,
+        where: { id: staleGame.id },
+      });
+    }
+  }
+
+  return [...gamesFromDatabase, ...missingGames];
+}
+
+function convertForDataBase(game: GameExtended) {
+  return {
+    id: game.id,
+    name: game.name,
+    thumbnail: game.thumbnail,
+    image: game.image,
+    year: game.year,
+    playingTime: game.playingTime,
+    minPlayers: game.minPlayers,
+    maxPlayers: game.maxPlayers,
+  };
+}
+
+export async function fetchGamesExtended(
+  gameIds: GameIds
+): Promise<GameExtended[]> {
+  if (!Array.isArray(gameIds)) {
+    return Promise.all([fetchGameData(gameIds)]);
+  } else {
+    return Promise.all(gameIds.map((id) => fetchGameData(id)));
+  }
+}
+
+async function fetchGameData(gameId: number): Promise<GameExtended> {
+  const parser = new XMLParser({
+    ignoreAttributes: false,
+  });
+  //TODO Check if this is a boardgame, the API actually returns RPGs and presumably videogames m)
+  return fetch(`https://api.geekdo.com/xmlapi/boardgame/${gameId}`, {
+    next: { revalidate: DAILY },
+  })
+    .then((response) => response.text())
+    .then((xml) => parser.parse(xml))
+    .then((bggGame) => {
+      return bggGame.boardgames;
+    })
+    .then((bggGame) => convertGame(bggGame));
+}
+
+function convertGame(boardgames: any): GameExtended {
+  return {
+    id: Number.parseInt(boardgames.boardgame["@_objectid"]),
+    name: getPrimaryName(boardgames.boardgame),
+    description: boardgames.boardgame.description,
+    thumbnail: boardgames.boardgame.thumbnail,
+    image: boardgames.boardgame.image,
+    year: boardgames.boardgame.yearpublished,
+    designers: getDesigners(boardgames.boardgame),
+    artists: getArtists(boardgames.boardgame),
+    playingTime: boardgames.boardgame.playingtime,
+    minPlayers: boardgames.boardgame.minplayers,
+    maxPlayers: boardgames.boardgame.maxplayers,
+  };
+}
+
+function getPrimaryName(bggGame: any): string {
+  if (Array.isArray(bggGame.name)) {
+    let found = bggGame.name[0]["#text"];
+    bggGame.name.forEach((name: any) => {
+      if (!!name["@_primary"]) {
+        found = name["#text"];
+      }
+    });
+    return found;
+  } else {
+    return bggGame.name["#text"];
+  }
+}
+
+function getDesigners(bggGame: any): string[] {
+  if (Array.isArray(bggGame.boardgamedesigner)) {
+    return bggGame.boardgamedesigner.map((designer: any) => designer["#text"]);
+  } else {
+    return [bggGame.boardgamedesigner["#text"]];
+  }
+}
+
+function getArtists(bggGame: any): string[] {
+  if (!bggGame.boardgameartist) {
+    return [];
+  } else if (Array.isArray(bggGame.boardgameartist)) {
+    return bggGame.boardgameartist.map((artist: any) => artist["#text"]);
+  } else {
+    console.log(bggGame.boardgameartist);
+    return [bggGame.boardgameartist["#text"]];
+  }
+}
