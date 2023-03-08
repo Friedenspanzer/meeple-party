3c7bc17975c6b92982e81ea59d01e680734c59c1
Pascal Simon <pascaltobiassimon@gmail.com>
2023-03-08 10:47:44 +0100

    Add relative time library

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,26 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
-"use client"
+"use client";
 
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import Link from "next/link";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en.json";
 
+TimeAgo.addDefaultLocale(en);
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const { user } = useUser();
   return (
     <>
-    <p>Public Meeple Party Front Page</p>
+      <p>Public Meeple Party Front Page</p>
       {!!user && (
         <p>
-          {user?.name} <a href="/api/auth/logout">Logout</a> <Link href="/app">Go to app</Link>
+          {user?.name} <a href="/api/auth/logout">Logout</a>{" "}
+          <Link href="/app">Go to app</Link>
         </p>
       )}
       {!user && (
