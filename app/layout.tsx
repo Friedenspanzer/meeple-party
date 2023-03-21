3da6f76965ee2d95fa065b0b646585f557edb264
Pascal Simon <pascaltobiassimon@gmail.com>
2023-03-21 16:26:56 +0100

    Remove unused code

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,30 +2,14 @@ import "./globals.css";
 import "@/theme/theme.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-import { headers } from "next/headers";
-import { Session } from "next-auth";
-
 import AuthProvider from "@/context/authContext";
 import UserProvider from "@/context/userContext";
-
-async function getSession(cookie: string): Promise<Session> {
-  const response = await fetch("/api/auth/session", {
-    headers: {
-      cookie,
-    },
-  });
-
-  const session = await response.json();
-
-  return Object.keys(session).length > 0 ? session : null;
-}
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = getSession(headers().get("cookie") ?? "");
   return (
     <html lang="en">
       <head />
