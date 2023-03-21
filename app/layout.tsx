841b16de0df603ac6355caf81987e431684480e4
Pascal Simon <pascaltobiassimon@gmail.com>
2023-03-21 17:03:53 +0100

    Add basic title definition

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,13 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import AuthProvider from "@/context/authContext";
 import UserProvider from "@/context/userContext";
 
+export const metadata = {
+  title: {
+    default: "Meeple Party",
+    template: "%s 🎲 Meeple Party",
+  },
+};
+
 export default async function RootLayout({
   children,
 }: {
