84b91b62993803987e67ca8ff971e3aacf6b4234
Pascal Simon <pascaltobiassimon@gmail.com>
2023-03-22 15:13:44 +0000

    Add validation and sanitization

--- pages/api/games/[gameId].ts
+++ pages/api/games/[gameId].ts
@@ -1,7 +1,7 @@
-import { prisma } from "@/db";
 import { fetchGames } from "@/utility/games";
 import { XMLParser } from "fast-xml-parser";
 import { NextApiRequest, NextApiResponse } from "next";
+import validator from "validator";
 
 export type SearchResult = {
   id: number;
@@ -18,12 +18,11 @@ export default async function handle(
 ) {
   try {
     if (req.method === "GET") {
-      //TODO Validation
       const { gameId } = req.query;
-      if (!gameId) {
-        throw new Error("No game ID given");
+      if (!gameId || Array.isArray(gameId) || !validator.isInt(gameId)) {
+        throw new Error("Error parsing game id.");
       }
-      const games = await fetchGames(Number.parseInt(gameId as string));
+      const games = await fetchGames(validator.toInt(gameId));
       if (!games || games.length === 0) {
         throw new Error("No game with that ID found");
       }
