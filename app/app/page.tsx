938379cadf1ee3638747fb8e6619a2aec8d66ce0
Pascal Greilach <pascaltobiassimon@gmail.com>
2023-04-01 15:04:48 +0200

    Add Games you could play right now to dashboard

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,3 +1,92 @@
-export default function App() {
-    return <h1>Meeple Party!</h1>;
+import GameCollection from "@/components/GameCollection/GameCollection";
+import { CollectionStatus } from "@/pages/api/collection/[gameId]";
+import {
+  GameCollection as GameCollectionType,
+  UserGameCollection,
+} from "@/datatypes/collection";
+import { Game } from "@/datatypes/game";
+import { getAllGamesOfFriends, getCollection } from "@/selectors/collections";
+import { getServerUser } from "@/utility/serverSession";
+
+export default async function App() {
+  const user = await getServerUser();
+  const myGameCollection = await getCollection(user.id);
+  const friendCollections = await getAllGamesOfFriends(user.id);
+  const collectedGames = myGameCollection.map((g) =>
+    collectGames(g, friendCollections)
+  );
+  return (
+    <>
+      <GameCollection
+        games={collectedGames
+          .filter(atLeastOnePersonOwns)
+          .filter(enoughPeopleWantToPlay)
+          .map(uncollectGames)}
+        showFriendCollection
+      >
+        <h2>Games you could play right now</h2>
+      </GameCollection>
+    </>
+  );
+}
+
+type CollectedGame = {
+  friendCollections: UserGameCollection[] | undefined;
+  game: Game;
+  status: {
+    own: boolean;
+    wantToPlay: boolean;
+    wishlist: boolean;
+  };
+};
+
+function collectGames(
+  myGame: GameCollectionType,
+  friendCollections: UserGameCollection[]
+) {
+  return {
+    ...myGame,
+    friendCollections: friendCollections.filter(
+      (g) => g.game.id === myGame.game.id
+    ),
+  };
+}
+
+function uncollectGames(collectedGame: CollectedGame): {
+  game: number | Game;
+  status?: CollectionStatus | undefined;
+} {
+  return {
+    game: collectedGame.game,
+    status: collectedGame.status,
+  };
+}
+
+function atLeastOnePersonOwns({
+  game,
+  status,
+  friendCollections,
+}: CollectedGame) {
+  return (
+    status.own ||
+    (friendCollections &&
+      friendCollections.find((f) => f.game.id === game.id && f.status.own))
+  );
+}
+
+function enoughPeopleWantToPlay({
+  game,
+  status,
+  friendCollections,
+}: CollectedGame) {
+  let count = 0;
+  if (status.wantToPlay) {
+    count++;
+  }
+  friendCollections?.forEach((c) => {
+    if (c.status.wantToPlay) {
+      count++;
+    }
+  });
+  return count >= game.minPlayers;
 }
--- datatypes/collection.ts
+++ datatypes/collection.ts
@@ -1,7 +1,21 @@
-import { PrivateUser } from "./userProfile";
+import { Game } from "./game";
+import { PrivateUser, PublicUser } from "./userProfile";
 
 export interface StatusByUser {
   own: PrivateUser[];
   wishlist: PrivateUser[];
   wantToPlay: PrivateUser[];
+}
+
+export interface GameCollection {
+  game: Game;
+  status: {
+    own: boolean;
+    wantToPlay: boolean;
+    wishlist: boolean;
+  };
+}
+
+export interface UserGameCollection extends GameCollection {
+  user: PrivateUser | PublicUser;
 }
