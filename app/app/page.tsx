c0ed14cf238dea9a4397204e4aa1e767f3052a2c
Pascal Simon <pascaltobiassimon@gmail.com>
2023-04-01 15:41:07 +0200

    Implement basic dashboard (#27)
    * Update page description
    
    * Add Games you could play right now to dashboard
    
    * Add games that are buy candidates to dashboard

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,3 +1,126 @@
-export default function App() {
-    return <h1>Meeple Party!</h1>;
+import GameCollection from "@/components/GameCollection/GameCollection";
+import { CollectionStatus } from "@/pages/api/collection/[gameId]";
+import {
+  GameCollection as GameCollectionType,
+  UserGameCollection,
+} from "@/datatypes/collection";
+import { Game } from "@/datatypes/game";
+import { getAllGamesOfFriends, getCollection } from "@/selectors/collections";
+import { getServerUser } from "@/utility/serverSession";
+import styles from "./dashboard.module.css";
+
+export default async function App() {
+  const user = await getServerUser();
+  const myGameCollection = await getCollection(user.id);
+  const friendCollections = await getAllGamesOfFriends(user.id);
+  const collectedGames = myGameCollection.map((g) =>
+    collectGames(g, friendCollections)
+  );
+  const gamesThatEnoughPeopleWantToPlay = collectedGames.filter(
+    enoughPeopleWantToPlay
+  );
+  return (
+    <>
+      <GameCollection
+        games={gamesThatEnoughPeopleWantToPlay
+          .filter(atLeastOnePersonOwns)
+          .map(uncollectGames)}
+        showFriendCollection
+        className={styles.collection}
+      >
+        <h2>Games you could play right now</h2>
+      </GameCollection>
+      <GameCollection
+        games={gamesThatEnoughPeopleWantToPlay
+          .filter(somebodyWantsToBuy)
+          .filter(nobodyOwns)
+          .map(uncollectGames)}
+        showFriendCollection
+        className={styles.collection}
+      >
+        <h2>Games somebody should probably buy already</h2>
+      </GameCollection>
+    </>
+  );
+}
+
+type CollectedGame = {
+  friendCollections: UserGameCollection[] | undefined;
+  game: Game;
+  status: {
+    own: boolean;
+    wantToPlay: boolean;
+    wishlist: boolean;
+  };
+};
+
+function collectGames(
+  myGame: GameCollectionType,
+  friendCollections: UserGameCollection[]
+) {
+  return {
+    ...myGame,
+    friendCollections: friendCollections.filter(
+      (g) => g.game.id === myGame.game.id
+    ),
+  };
+}
+
+function uncollectGames(collectedGame: CollectedGame): {
+  game: number | Game;
+  status?: CollectionStatus | undefined;
+} {
+  return {
+    game: collectedGame.game,
+    status: collectedGame.status,
+  };
+}
+
+function atLeastOnePersonOwns({
+  game,
+  status,
+  friendCollections,
+}: CollectedGame) {
+  return (
+    status.own ||
+    (friendCollections &&
+      friendCollections.find((f) => f.game.id === game.id && f.status.own))
+  );
+}
+
+function nobodyOwns({ game, status, friendCollections }: CollectedGame) {
+  return (
+    !status.own &&
+    friendCollections &&
+    !friendCollections.find((f) => f.game.id === game.id && f.status.own)
+  );
+}
+
+function enoughPeopleWantToPlay({
+  game,
+  status,
+  friendCollections,
+}: CollectedGame) {
+  let count = 0;
+  if (status.wantToPlay) {
+    count++;
+  }
+  friendCollections?.forEach((c) => {
+    if (c.status.wantToPlay) {
+      count++;
+    }
+  });
+  return count >= game.minPlayers;
+}
+
+function somebodyWantsToBuy({
+  game,
+  status,
+  friendCollections,
+}: CollectedGame) {
+  return (
+    status.wishlist ||
+    (friendCollections &&
+      friendCollections.find((f) => f.game.id === game.id && f.status.wishlist))
+  );
 }
--- selectors/collections.ts
+++ selectors/collections.ts
@@ -1,7 +1,32 @@
-import { StatusByUser } from "@/datatypes/collection";
+import {
+  GameCollection,
+  StatusByUser,
+  UserGameCollection,
+} from "@/datatypes/collection";
+import { Game as CleanGame } from "@/datatypes/game";
 import { PrivateUser } from "@/datatypes/userProfile";
 import { prisma } from "@/db";
-import { GameCollection, RelationshipType, User } from "@prisma/client";
+import {
+  Game,
+  GameCollection as PrismaGameCollection,
+  RelationshipType,
+  User,
+} from "@prisma/client";
+
+export async function getCollection(userId: string): Promise<GameCollection[]> {
+  const result = await prisma.gameCollection.findMany({
+    where: { userId },
+    include: { game: true, user: true },
+  });
+  return result.map((r) => ({
+    game: cleanGame(r.game),
+    status: {
+      own: r.own,
+      wantToPlay: r.wantToPlay,
+      wishlist: r.wishlist,
+    },
+  }));
+}
 
 export async function getCollectionStatusOfFriends(
   gameId: number,
@@ -36,8 +61,47 @@ export async function getCollectionStatusOfFriends(
   return convertToStatusByUser(result);
 }
 
+export async function getAllGamesOfFriends(
+  userId: string
+): Promise<UserGameCollection[]> {
+  const result = await prisma.gameCollection.findMany({
+    where: {
+      user: {
+        OR: [
+          {
+            receivedRelationships: {
+              some: {
+                senderId: userId,
+                type: RelationshipType.FRIENDSHIP,
+              },
+            },
+          },
+          {
+            sentRelationships: {
+              some: {
+                recipientId: userId,
+                type: RelationshipType.FRIENDSHIP,
+              },
+            },
+          },
+        ],
+      },
+    },
+    include: { user: true, game: true },
+  });
+  return result.map((r) => ({
+    user: r.user,
+    game: cleanGame(r.game),
+    status: {
+      own: r.own,
+      wantToPlay: r.wantToPlay,
+      wishlist: r.wishlist,
+    },
+  }));
+}
+
 function convertToStatusByUser(
-  dbResult: (GameCollection & {
+  dbResult: (PrismaGameCollection & {
     user: User;
   })[]
 ): StatusByUser {
@@ -55,4 +119,9 @@ function convertToStatusByUser(
 function convertToPrivateUser(dbUser: User): PrivateUser {
   const { email, emailVerified, bggName, profileComplete, ...user } = dbUser;
   return user;
+}
+
+function cleanGame(uncleanGame: Game): CleanGame {
+  const { updatedAt, ...cleanGame } = uncleanGame;
+  return cleanGame;
 }
