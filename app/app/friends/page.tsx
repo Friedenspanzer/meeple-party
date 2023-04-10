1a4a5b603758761680da377f7d2abaa9594c8624
Pascal Greilach <pascaltobiassimon@gmail.com>
2023-04-10 16:00:30 +0200

    Move friend collection updates into card

--- app/app/friends/page.tsx
+++ app/app/friends/page.tsx
@@ -1,8 +1,11 @@
 import GamePill from "@/components/GamePill/GamePill";
+import Person from "@/components/Person/Person";
 import { prisma } from "@/db";
 import { getFriends } from "@/selectors/relationships";
 import { getServerUser } from "@/utility/serverSession";
+import classNames from "classnames";
 import Link from "next/link";
+import styles from "./friends.module.css";
 
 export default async function Friends() {
   const user = await getServerUser();
@@ -30,29 +33,46 @@ export default async function Friends() {
           .filter((u) => u.wantToPlay)
           .map((u) => u.game);
         return (
-          <>
-            <h3 key={friend.id}>{friend.name}</h3>
-            {friendOwns.length > 0 && (
-              <>
-                <h4>Owns</h4>
-                {friendOwns.map((g) => (
-                  <Link href={`/app/game/${g.id}`} key={g.id}>
-                    <GamePill game={g} />
-                  </Link>
-                ))}
-              </>
-            )}
-            {friendWantsToPlay.length > 0 && (
-              <>
-                <h4>Wants to play</h4>
-                {friendWantsToPlay.map((g) => (
-                  <Link href={`/app/game/${g.id}`} key={g.id}>
-                    <GamePill game={g} />
-                  </Link>
-                ))}
-              </>
-            )}
-          </>
+          <div className={classNames("card", styles.card)} key={friend.id}>
+            <div className="card-header">
+              <Person
+                key={friend.id}
+                name={friend.name!}
+                realName={friend.realName || undefined}
+                image={friend.image || undefined}
+              />
+            </div>
+            <div className="card-body">
+              {friendOwns.length > 0 && (
+                <>
+                  <h4>Owns</h4>
+                  {friendOwns.map((g) => (
+                    <Link
+                      href={`/app/game/${g.id}`}
+                      key={g.id}
+                      className={styles.link}
+                    >
+                      <GamePill game={g} />
+                    </Link>
+                  ))}
+                </>
+              )}
+              {friendWantsToPlay.length > 0 && (
+                <>
+                  <h4 className={styles.wantToPlay}>Wants to play</h4>
+                  {friendWantsToPlay.map((g) => (
+                    <Link
+                      href={`/app/game/${g.id}`}
+                      key={g.id}
+                      className={styles.link}
+                    >
+                      <GamePill game={g} />
+                    </Link>
+                  ))}
+                </>
+              )}
+            </div>
+          </div>
         );
       })}
       <h2>All your friends</h2>
