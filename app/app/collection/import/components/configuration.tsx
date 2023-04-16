c6f9425f8b94fe641b0df3cbae74b322cbf9d158
Pascal Greilach <pascaltobiassimon@gmail.com>
2023-04-16 23:21:47 +0200

    Make configuration step responsive

--- app/app/collection/import/components/configuration.tsx
+++ app/app/collection/import/components/configuration.tsx
@@ -6,16 +6,18 @@ export interface ConfigurationProps {
 
 const Configuration: React.FC<ConfigurationProps> = (props) => {
   return (
-    <>
-      <p>Currently there is nothing to do here.</p>
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={(e) => props.onDone({})}
-      >
-        Go on then! <i className="bi bi-caret-right-square"></i>
-      </button>
-    </>
+    <div className="row align-items-center g-4">
+      <div className="col-md-auto">Currently there is nothing to do here.</div>
+      <div className="col">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={(e) => props.onDone({})}
+        >
+          Go on then! <i className="bi bi-caret-right-square"></i>
+        </button>
+      </div>
+    </div>
   );
 };
 
