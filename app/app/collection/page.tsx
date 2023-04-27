3704ffb5e22ae62b7487f520af995cf6d4f759d0
Pascal Greilach <pascaltobiassimon@gmail.com>
2023-04-27 10:08:56 +0200

    Move sort out of return value

--- app/app/collection/page.tsx
+++ app/app/collection/page.tsx
@@ -8,23 +8,23 @@ import { Game } from "@prisma/client";
 
 export default async function Collection() {
   const user = await getServerUser();
-  const gameCollection = await prisma.gameCollection.findMany({
-    where: { userId: user.id },
-    include: { game: true },
-  });
+  const gameCollection = (
+    await prisma.gameCollection.findMany({
+      where: { userId: user.id },
+      include: { game: true },
+    })
+  ).sort((a, b) => (a.game.name > b.game.name ? 1 : -1));
   const friendCollections = await getMultipleCollectionStatusOfFriends(
     gameCollection.map((g) => g.gameId),
     user.id
   );
   return (
     <GameCollection
-      games={gameCollection
-        .sort((a, b) => (a.game.name > b.game.name ? 1 : -1))
-        .map(({ game, own, wantToPlay, wishlist }) => ({
-          game: cleanGame(game),
-          status: { own, wantToPlay, wishlist },
-          friendCollections: findFriendCollection(game.id, friendCollections),
-        }))}
+      games={gameCollection.map(({ game, own, wantToPlay, wishlist }) => ({
+        game: cleanGame(game),
+        status: { own, wantToPlay, wishlist },
+        friendCollections: findFriendCollection(game.id, friendCollections),
+      }))}
       defaultFilter={{ ...emptyFilter, collectionStatus: { own: true } }}
       showFriendCollection={true}
     />
