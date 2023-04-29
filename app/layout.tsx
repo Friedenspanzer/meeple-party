30d3b17b68667124c98ead169b6ea2cccf8af805
Pascal Greilach <pascaltobiassimon@gmail.com>
2023-04-29 14:55:27 +0200

    Only send analytics data when flag is set

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import AuthProvider from "@/context/authContext";
 import UserProvider from "@/context/userContext";
 import Script from "next/script";
 import Analytics from "@/components/Analytics/Analytics";
+import { getServerUser } from "@/utility/serverSession";
+import { getUserPreferences } from "@/utility/userProfile";
 
 export const metadata = {
   title: {
@@ -18,6 +20,8 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const user = await getServerUser();
+  const sendAnalytics = getUserPreferences(user).sendAnalyticsData;
   return (
     <html lang="en">
       <head />
@@ -26,7 +30,7 @@ export default async function RootLayout({
         <AuthProvider>
           <UserProvider>{children}</UserProvider>
         </AuthProvider>
-        <Analytics />
+        {sendAnalytics && <Analytics />}
       </body>
       <Script src="/bootstrap.bundle.min.js" />
     </html>
