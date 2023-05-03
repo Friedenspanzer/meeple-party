13ff7ccd25be5044a1ae89b0c01ebed3b1f93a9f
Pascal Greilach <pascaltobiassimon@gmail.com>
2023-05-03 07:50:21 +0200

    Determine in client if analytics should be send
    Before the Analytics component was rendered on the server and did not have access to the current user details, so even after updating the user data in the client a full page refresh would have been necessary to change the setting.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,6 @@ import AuthProvider from "@/context/authContext";
 import UserProvider from "@/context/userContext";
 import Script from "next/script";
 import Analytics from "@/components/Analytics/Analytics";
-import { getServerUser } from "@/utility/serverSession";
-import { getUserPreferences } from "@/utility/userProfile";
 
 export const metadata = {
   title: {
@@ -20,17 +18,17 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const user = await getServerUser();
-  const sendAnalytics = getUserPreferences(user).sendAnalyticsData;
   return (
     <html lang="en">
       <head />
       <body>
         <Script src="/bootstrap.bundle.min.js" />
         <AuthProvider>
-          <UserProvider>{children}</UserProvider>
+          <UserProvider>
+            {children}
+            <Analytics />
+          </UserProvider>
         </AuthProvider>
-        {sendAnalytics && <Analytics />}
       </body>
       <Script src="/bootstrap.bundle.min.js" />
     </html>
--- components/Analytics/Analytics.tsx
+++ components/Analytics/Analytics.tsx
@@ -1,7 +1,22 @@
 "use client";
 
+import { useUser } from "@/context/userContext";
+import { getUserPreferences } from "@/utility/userProfile";
 import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
+import { useCallback } from "react";
 
 export default function Analytics() {
-  return <VercelAnalytics />;
+  const { user, loading } = useUser();
+
+  const beforeSend = useCallback(
+    (event: any) => {
+      if (loading || !user || !getUserPreferences(user).sendAnalyticsData) {
+        return null;
+      }
+      return event;
+    },
+    [user, loading]
+  );
+
+  return <VercelAnalytics beforeSend={beforeSend} />;
 }
