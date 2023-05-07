2cb222544985d69593772b0c3945e6e89e58e9f6
Pascal Greilach <pascaltobiassimon@gmail.com>
2023-05-07 14:37:26 +0200

    Add error handling to app layout
    This should fix at least some cases of #30 occuring.

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,21 +1,26 @@
 import AppContainer from "@/components/AppContainer/AppContainer";
 import { useFeatureFlagServer } from "@/utility/featureFlag";
+import { notFound } from "next/navigation";
 
 export default async function AppLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const showApp = await useFeatureFlagServer("show_app");
-  return (
-    <>
-      {showApp && <AppContainer>{children}</AppContainer>}
-      {!showApp && (
-        <>
-          <h1>Meeple Party is currently invite-only</h1>
-          <p>Please ask the administrator to unlock your account.</p>
-        </>
-      )}
-    </>
-  );
+  try {
+    const showApp = await useFeatureFlagServer("show_app");
+    return (
+      <>
+        {showApp && <AppContainer>{children}</AppContainer>}
+        {!showApp && (
+          <>
+            <h1>Meeple Party is currently invite-only</h1>
+            <p>Please ask the administrator to unlock your account.</p>
+          </>
+        )}
+      </>
+    );
+  } catch {
+    notFound();
+  }
 }
