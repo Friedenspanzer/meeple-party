9b0ed2ba08415bc730ad88a8aff1665d57e308e1
Pascal Greilach <pascaltobiassimon@gmail.com>
2023-05-13 15:04:17 +0200

    New design for the front page
    Still incomplete obviously

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,216 @@
 import Login from "@/app/Login/Login";
 import classNames from "classnames";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={classNames("container-fluid", styles.header)}>
-      <div className="row align-items-center g-5 p-5">
-        <div className="col-md-3">
-          <Image
-            src="/logo.svg"
-            width={300}
-            height={300}
-            alt="Meeple Party"
-            className={styles.image}
-          />
-        </div>
-        <div className="col-md-6">
-          <h1 className="display-1">Meeple Party</h1>
-          <h2 className="display-6">
-            The place to meet your friends for board games
-          </h2>
-        </div>
-        <div className="col-md-3">
-          <Login />
+    <>
+      <div className={classNames("container-fluid", styles.highlight)}>
+        <div className="row align-items-center g-5 p-5">
+          <div className="col-md-3">
+            <Image
+              src="/logo.svg"
+              width={300}
+              height={300}
+              alt="Meeple Party"
+              className={styles.image}
+            />
+          </div>
+          <div className="col-md-6">
+            <h1 className="display-1">Meeple Party</h1>
+            <h2 className="display-6">
+              The place to browse your friend&apos;s collections.
+            </h2>
+          </div>
+          <div className="col-md-3">
+            <Login />
+          </div>
         </div>
       </div>
-    </div>
+      <div className="container">
+        <div className="row mt-4 g-4">
+          <div className="col-4">
+            <div className="card text-bg-primary">
+              <Image
+                src="/highlight-card-1.png"
+                className={classNames("card-img-top", styles.feature)}
+                alt="Screenshot of a cool feature"
+                width={300}
+                height={300}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Cool feature</h5>
+                <p className="card-text">Look at this cool feature!</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="card text-bg-primary">
+              <Image
+                src="/highlight-card-2.png"
+                className={classNames("card-img-top", styles.feature)}
+                alt="Screenshot of a cool feature"
+                width={300}
+                height={300}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Cool feature</h5>
+                <p className="card-text">Look at this cool feature!</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="card text-bg-primary">
+              <Image
+                src="/highlight-card-3.png"
+                className={classNames("card-img-top", styles.feature)}
+                alt="Screenshot of a cool feature"
+                width={300}
+                height={300}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Cool feature</h5>
+                <p className="card-text">Look at this cool feature!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-4">
+          <div className={classNames("col-1", styles.number)}>
+            <i className="bi bi-1-circle-fill"></i>
+          </div>
+          <div className={classNames("col", styles.instruction)}>
+            <h3 className={classNames(styles.instructionTitle)}>
+              Fill your collection
+            </h3>
+            <p>
+              Tell us what games you own. We know, for most people this is
+              busywork. But trust us that it will be worth it. Or you can do it
+              later, one game at a time. If you already maintain your collection
+              on BoardGameGeek you can just import it.
+            </p>
+          </div>
+        </div>
+        <div className="row mt-4">
+          <div className={classNames("col-1", styles.number)}>
+            <i className="bi bi-2-circle-fill"></i>
+          </div>
+          <div className={classNames("col", styles.instruction)}>
+            <h3 className={classNames(styles.instructionTitle)}>
+              Find your friends and check out their games
+            </h3>
+            <p>
+              Connect with the people you already know. Meeple Party is not a
+              social network like the others, we don&apos;t even want you to
+              befriend people you haven&apos; met. Just fill in the circle of
+              friends and acquaintances you already play games with.
+            </p>
+            <p>
+              Then have a look at the games they own and just mark every game
+              you want to play. Meeple Party will do the rest for you and tell
+              you exactly what game that somebody owns enough people in your
+              circle of friends want to play.
+            </p>
+          </div>
+        </div>
+        <div className="row mt-4">
+          <div className={classNames("col-1", styles.number)}>
+            <i className="bi bi-3-circle-fill"></i>
+          </div>
+          <div className={classNames("col", styles.instruction)}>
+            <h3 className={classNames(styles.instructionTitle)}>
+              Go play some games
+            </h3>
+            <p>
+              Tell your friends you found some matches and fix a date for a game
+              night. Everybody can just look up what games to bring.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div
+        className={classNames("container-fluid pt-4 pb-4", styles.highlight)}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <h2>Pricing</h2>
+              <p>
+                Meeple Party is currently free and there will always be a free
+                version available. We may however add a premium account system
+                one day but we swear no features you currently have will be
+                taken away from you in the process.
+              </p>
+            </div>
+            <div className="col-md-6">
+              <h2>Free and open-source</h2>
+              <p>
+                Meeple party is free and open-source and will always remain so.
+                Every new feature will always be added to the main project.
+                There will be no closed-source additions and you will always be
+                free to host your own version of Meeple Party with all the
+                features wihtout paying us a dime.
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>Everybody is welcome</h2>
+              <p>
+                Gaming is for everybody and Meeple Party is for everybody. We
+                pledge to protect vulnerable and marginalized people as best as
+                we can. We expected everybody to demonstrate empathy and
+                kindness and will never tolerate harassment, insults, hate
+                speech or behaviour that endangers others.
+              </p>
+            </div>
+            <div className="col-md-6">
+              <h2>Transparency</h2>
+              <p>
+                We try to run and develop Meeple Party as openly as is feasible
+                and include the community on every step of the way.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col">
+            <h2>Get in touch</h2>
+          </div>
+        </div>
+        <div className="row mt-4">
+          <div className="col-6">
+            <h3>Message us</h3>
+          </div>
+          <div className="col-3">
+            <h3>Social media</h3>
+            <ul className={styles.social}>
+              <li>
+                <Link href="." title="Mastodon">
+                  <i className="bi bi-mastodon"></i>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://github.com/Friedenspanzer/meeple-party/"
+                  title="Github"
+                >
+                  <i className="bi bi-github"></i>
+                </Link>
+              </li>
+              <li>
+                <Link href="." title="Reddit">
+                  <i className="bi bi-reddit"></i>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
