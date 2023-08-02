83993fbadbc501dcf51e4162af9bc3bd832be468
Pascal Greilach <pascaltobiassimon@gmail.com>
2023-08-02 22:17:16 +0200

    Add switch to toggle real name visibility

--- app/app/profile/edit/privacy/page.tsx
+++ app/app/profile/edit/privacy/page.tsx
@@ -1,12 +1,21 @@
 import SendAnalyticsData from "@/components/AutoUpdateToggle/Toggles/SendAnalyticsData";
+import ShowRealName from "@/components/AutoUpdateToggle/Toggles/ShowRealName";
 
 export const metadata = {
-  title: 'Privacy settings',
-}
+  title: "Privacy settings",
+};
 
 export default async function Privacy() {
   return (
     <>
+      <div className="row">
+        <div className="col offset-md-2">
+          <h2>Profile information visibility</h2>
+        </div>
+      </div>
+
+      <ShowRealName />
+
       <div className="row">
         <div className="col offset-md-2">
           <h2>Tracking</h2>
