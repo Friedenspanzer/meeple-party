2f1cfc8ca46ea171e68339818ff6d41985437385
Pascal Greilach <pascaltobiassimon@gmail.com>
2023-08-16 15:03:42 +0200

    Add tests for relationship DELETE API

--- app/api/v2/relationship/[userId]/route.test.ts
+++ app/api/v2/relationship/[userId]/route.test.ts
@@ -247,4 +247,180 @@ describe("DELETE relationship/[userId]", () => {
 
     expect(result.status).toBe(500);
   });
+  it("deletes the relationship", async () => {
+    const otherUser = generatePrismaUser();
+
+    const relationship = generateFullPrismaRelationship(
+      PrismaRelationshipType.FRIENDSHIP,
+      myUser,
+      otherUser
+    );
+
+    prismaMock.relationship.findMany.mockResolvedValue([relationship]);
+
+    const result = await DELETE(requestMock, {
+      params: { userId: otherUser.id },
+    });
+
+    expect(result.status).toBe(200);
+
+    expect(prismaMock.relationship.findMany).toHaveBeenCalledWith({
+      where: {
+        OR: [
+          { recipientId: myUser.id, senderId: otherUser.id },
+          { recipientId: otherUser.id, senderId: myUser.id },
+        ],
+      },
+      include: {
+        recipient: true,
+        sender: true,
+      },
+    });
+    expect(prismaMock.relationship.delete).toHaveBeenCalledWith({
+      where: {
+        senderId_recipientId: {
+          recipientId: relationship.recipientId,
+          senderId: relationship.senderId,
+        },
+      },
+    });
+  });
+  it("returns an existing friendship", async () => {
+    const otherUser = generatePrismaUser();
+    const expectedUser: UserProfile = convertToUserProfile(otherUser, true);
+
+    const relationship = generateFullPrismaRelationship(
+      PrismaRelationshipType.FRIENDSHIP,
+      myUser,
+      otherUser
+    );
+
+    prismaMock.relationship.findMany.mockResolvedValue([relationship]);
+
+    const result = await DELETE(requestMock, {
+      params: { userId: otherUser.id },
+    });
+
+    expect(result.status).toBe(200);
+
+    const data = (await result.json()) as RelationshipGetResult;
+
+    expect(data.normalizedRelationship.profile).toEqual(expectedUser);
+    expect(data.normalizedRelationship.type).toEqual(
+      RelationshipType.FRIENDSHIP
+    );
+    expect(new Date(data.normalizedRelationship.lastUpdate)).toEqual(
+      relationship.updatedAt
+    );
+
+    expect(prismaMock.relationship.findMany).toHaveBeenCalledWith({
+      where: {
+        OR: [
+          { recipientId: myUser.id, senderId: otherUser.id },
+          { recipientId: otherUser.id, senderId: myUser.id },
+        ],
+      },
+      include: {
+        recipient: true,
+        sender: true,
+      },
+    });
+  });
+  it("returns an existing reverse friendship", async () => {
+    const otherUser = generatePrismaUser();
+    const expectedUser: UserProfile = convertToUserProfile(otherUser, true);
+
+    const relationship = generateFullPrismaRelationship(
+      PrismaRelationshipType.FRIENDSHIP,
+      otherUser,
+      myUser
+    );
+
+    prismaMock.relationship.findMany.mockResolvedValue([relationship]);
+
+    const result = await DELETE(requestMock, {
+      params: { userId: otherUser.id },
+    });
+
+    expect(result.status).toBe(200);
+
+    const data = (await result.json()) as RelationshipGetResult;
+
+    expect(data.normalizedRelationship.profile).toEqual(expectedUser);
+    expect(data.normalizedRelationship.type).toEqual(
+      RelationshipType.FRIENDSHIP
+    );
+    expect(new Date(data.normalizedRelationship.lastUpdate)).toEqual(
+      relationship.updatedAt
+    );
+
+    expect(prismaMock.relationship.findMany).toHaveBeenCalledWith({
+      where: {
+        OR: [
+          { recipientId: myUser.id, senderId: otherUser.id },
+          { recipientId: otherUser.id, senderId: myUser.id },
+        ],
+      },
+      include: {
+        recipient: true,
+        sender: true,
+      },
+    });
+  });
+  it("returns an existing sent friend request", async () => {
+    const otherUser = generatePrismaUser();
+    const expectedUser: UserProfile = convertToUserProfile(otherUser, false);
+
+    const relationship = generateFullPrismaRelationship(
+      PrismaRelationshipType.FRIEND_REQUEST,
+      myUser,
+      otherUser
+    );
+
+    prismaMock.relationship.findMany.mockResolvedValue([relationship]);
+
+    const result = await DELETE(requestMock, {
+      params: { userId: otherUser.id },
+    });
+
+    expect(result.status).toBe(200);
+
+    const data = (await result.json()) as RelationshipGetResult;
+
+    expect(data.normalizedRelationship.profile).toEqual(expectedUser);
+    expect(data.normalizedRelationship.type).toEqual(
+      RelationshipType.FRIEND_REQUEST_SENT
+    );
+    expect(new Date(data.normalizedRelationship.lastUpdate)).toEqual(
+      relationship.updatedAt
+    );
+  });
+  it("returns an existing received friend request", async () => {
+    const otherUser = generatePrismaUser();
+    const expectedUser: UserProfile = convertToUserProfile(otherUser, false);
+
+    const relationship = generateFullPrismaRelationship(
+      PrismaRelationshipType.FRIEND_REQUEST,
+      otherUser,
+      myUser
+    );
+
+    prismaMock.relationship.findMany.mockResolvedValue([relationship]);
+
+    const result = await DELETE(requestMock, {
+      params: { userId: otherUser.id },
+    });
+
+    expect(result.status).toBe(200);
+
+    const data = (await result.json()) as RelationshipGetResult;
+
+    expect(data.normalizedRelationship.profile).toEqual(expectedUser);
+    expect(data.normalizedRelationship.type).toEqual(
+      RelationshipType.FRIEND_REQUEST_RECEIVED
+    );
+    expect(new Date(data.normalizedRelationship.lastUpdate)).toEqual(
+      relationship.updatedAt
+    );
+  });
 });
