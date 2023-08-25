7be75437d839b64f5ba0257b4b6bfbb4d8ff59b7
Pascal Greilach <pascaltobiassimon@gmail.com>
2023-08-25 15:11:05 +0200

    Translate collection view

--- app/app/collection/page.tsx
+++ app/app/collection/page.tsx
@@ -1,23 +1,28 @@
 import GameCollection from "@/components/GameCollection/GameCollection";
 import { prisma } from "@/db";
+import { getTranslation } from "@/i18n";
 import { getMultipleCollectionStatusOfFriends } from "@/selectors/collections";
 import { findFriendCollection } from "@/utility/collections";
 import { emptyFilter } from "@/utility/filter";
 import { getServerUser } from "@/utility/serverSession";
 import { Game } from "@prisma/client";
+import { Metadata, ResolvingMetadata } from "next";
 
-export const metadata = {
-  title: 'Your collection',
+export async function generateMetadata(
+  parent?: ResolvingMetadata
+): Promise<Metadata> {
+  const { t } = await getTranslation("collection");
+  return {
+    title: t("Title"),
+  };
 }
 
 export default async function Collection() {
   const user = await getServerUser();
-  const gameCollection = (
-    await prisma.gameCollection.findMany({
-      where: { userId: user.id },
-      include: { game: true },
-    })
-  );
+  const gameCollection = await prisma.gameCollection.findMany({
+    where: { userId: user.id },
+    include: { game: true },
+  });
   const friendCollections = await getMultipleCollectionStatusOfFriends(
     gameCollection.map((g) => g.gameId),
     user.id
