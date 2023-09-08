82ac986cde39f13632639e3927cf29c865e6e84f
Pascal Greilach <pascaltobiassimon@gmail.com>
2023-09-08 10:13:39 +0200

    Add function to update user preferences to hook

--- hooks/useUserPreferences.ts
+++ hooks/useUserPreferences.ts
@@ -6,35 +6,63 @@ import {
   UserPreferences,
 } from "@/datatypes/userProfile";
 import { getUserPreferences } from "@/utility/userProfile";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useCallback, useMemo } from "react";
 
 type UseUserPreferencesResult =
   | {
       preferences: UserPreferences;
       loading: false;
+      update: (preferences: Partial<UserPreferences>) => Promise<void>;
     }
   | {
       preferences: undefined;
       loading: true;
+      update: (preferences: Partial<UserPreferences>) => Promise<void>;
     };
 
 export function useUserPreferences(): UseUserPreferencesResult {
-  const { user, loading } = useUser();
+  const { user, loading, update: updateUser } = useUser();
+
+  const update = useCallback(
+    (preferences: Partial<UserPreferences>) => {
+      if (user) {
+        return axios
+          .patch("/api/user", {
+            preferences: {
+              ...getUserPreferences(user),
+              ...preferences,
+            },
+          })
+          .then(() => {
+            updateUser();
+          });
+      } else {
+        return Promise.reject();
+      }
+    },
+    [updateUser, user]
+  );
 
   if (loading) {
     return {
       preferences: undefined,
       loading,
+      update,
     };
   } else {
     if (!user) {
       return {
         preferences: defaultUserPreferences,
         loading: false,
+        update,
       };
     } else {
       return {
         preferences: getUserPreferences(user),
         loading: false,
+        update,
       };
     }
   }
