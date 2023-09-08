57c649345ce88ef7fd4437890e145442aeec36f8
Pascal Greilach <pascaltobiassimon@gmail.com>
2023-09-08 10:16:00 +0200

    Add language picker to user settings

--- app/app/profile/edit/settings/page.tsx
+++ app/app/profile/edit/settings/page.tsx
@@ -1,14 +1,38 @@
 import DeleteAccount from "@/components/DeleteAccount/DeleteAccount";
+import LanguagePicker from "@/components/LanguagePicker/LanguagePicker";
 
 export default function Page() {
   return (
-    <>
+    <div className="grid">
       <div className="row">
+        <div className="col-8 offset-md-2">
+          <h2>Language</h2>
+        </div>
+      </div>
+      <div className="row mt-2">
+        <div className="col-8 offset-md-2">
+          <h3>Page</h3>
+          <LanguagePicker
+            availableLanguages={["auto", "en", "de"]}
+            type="page"
+          />
+        </div>
+      </div>
+      <div className="row mt-2">
+        <div className="col-8 offset-md-2">
+          <h3>Games</h3>
+          <LanguagePicker
+            availableLanguages={["follow", "auto", "en"]}
+            type="game"
+          />
+        </div>
+      </div>
+      <div className="row mt-4">
         <div className="col-8 offset-md-2">
           <h2>Danger Zone</h2>
         </div>
       </div>
-      <div className="row">
+      <div className="row mt-2">
         <div className="col-8 offset-md-2">
           <h3>Account deletion</h3>
         </div>
@@ -18,6 +42,6 @@ export default function Page() {
           <DeleteAccount />
         </div>
       </div>
-    </>
+    </div>
   );
 }
