93975b7bd49b4066f1af51a006522e51ce55467e
Pascal Simon <pascaltobiassimon@gmail.com>
2023-11-24 15:30:27 +0100

    Use icons as game box size pickers

--- components/GameBoxSizePicker/GameBoxSizePicker.tsx
+++ components/GameBoxSizePicker/GameBoxSizePicker.tsx
@@ -1,5 +1,7 @@
 import useGameBoxSize, { GameBoxSize } from "@/hooks/useGameBoxSize";
 import { SegmentedControl } from "@mantine/core";
+import IconGameBoxBig from "../icons/GameboxBig";
+import IconGameBoxMedium from "../icons/GameboxMedium";
 
 export default function GameBoxSizePicker({
   className,
@@ -12,8 +14,8 @@ export default function GameBoxSizePicker({
       value={size}
       onChange={(value) => setSize(value as GameBoxSize)}
       data={[
-        { label: "MD", value: "md" },
-        { label: "XL", value: "xl" },
+        { label: <IconGameBoxMedium />, value: "md" },
+        { label: <IconGameBoxBig />, value: "xl" },
       ]}
       className={className}
     />
