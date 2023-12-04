fd7ba605b23a528185e2c3a25753fd699863a62d
Pascal Greilach <pascal@greilach.dev>
2023-12-04 21:36:59 +0000

    Update Jest to generate coverage data

--- jest.config.mjs
+++ jest.config.mjs
@@ -16,6 +16,7 @@ const config = {
     "^@/(.*)$": "<rootDir>/$1",
   },
   testRegex: "^(.*)/(.*)\\.(test)\\.[jt]sx?$",
+  collectCoverage: true,
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
