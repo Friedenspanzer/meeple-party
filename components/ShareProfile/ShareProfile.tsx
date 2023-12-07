8a093b286af0a42ef357ce77ffcece86f1a9fb4e
Pascal Simon <pascaltobiassimon@gmail.com>
2023-12-08 00:16:37 +0100

    Finalize basic implementation
    Still needs tests, some UI work and a fallback.

--- components/ShareProfile/ShareProfile.tsx
+++ components/ShareProfile/ShareProfile.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useTranslation } from "@/i18n/client";
 import { Button, Group } from "@mantine/core";
+import { IconShare2 } from "@tabler/icons-react";
 import { useCallback } from "react";
 
 export interface ShareProfileProps {
@@ -8,17 +10,19 @@ export interface ShareProfileProps {
 }
 
 export default function ShareProfile({ profileId }: ShareProfileProps) {
+  const { t } = useTranslation("profile");
   const share = useCallback(() => {
-    if (navigator.canShare()) {
-      navigator.share({
-        url: `${process.env.BASE_URL}/app/profile/${profileId}`,
-      });
+    const shareData: ShareData = {
+      url: `${process.env.BASE_URL}/app/profile/${profileId}`
+    };
+    if (navigator.canShare(shareData)) {
+      navigator.share(shareData);
     }
   }, [profileId]);
   return (
     <Group>
-      <Button variant="filled" onClick={share}>
-        Button
+      <Button variant="filled" onClick={share} leftSection={<IconShare2 />}>
+        {t("Share")}
       </Button>
     </Group>
   );
