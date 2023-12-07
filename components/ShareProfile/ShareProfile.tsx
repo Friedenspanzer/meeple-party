41fd6b971913920278bb03873710ae27746e1ab9
Pascal Simon <pascaltobiassimon@gmail.com>
2023-12-07 23:49:20 +0100

    Add basic functionality to share button

--- components/ShareProfile/ShareProfile.tsx
+++ components/ShareProfile/ShareProfile.tsx
@@ -1,11 +1,22 @@
+"use client";
+
+import { useCallback } from "react";
+
 export interface ShareProfileProps {
   profileId: string;
 }
 
 export default function ShareProfile({ profileId }: ShareProfileProps) {
+  const share = useCallback(() => {
+    if (navigator.canShare()) {
+      navigator.share({
+        url: `${process.env.BASE_URL}/app/profile/${profileId}`,
+      });
+    }
+  }, [profileId]);
   return (
-    <button type="button" className="btn btn-primary">
-      Share
+    <button type="button" className="btn btn-primary" onClick={share}>
+      <i className="bi bi-share"></i>&nbsp;Share profile
     </button>
   );
 }
