bb078bfe90c04c4d3ae348269de4d4abbaf98116
Pascal Simon <pascaltobiassimon@gmail.com>
2023-12-08 00:05:52 +0100

    Configure Mantine theme colors
    Welcoming Meeple Party Purple and Meeple Party Pink, or Murple and Mink for short!

--- theme/mantine.ts
+++ theme/mantine.ts
@@ -1,3 +1,13 @@
+"use client";
+
+import { generateColors } from "@mantine/colors-generator";
 import { createTheme } from "@mantine/core";
 
-export const theme = createTheme({});
+export const theme = createTheme({
+  colors: {
+    murple: generateColors("hsl(269, 80%, 56%)"),
+    mink: generateColors("rgb(238, 102, 217)"),
+  },
+  primaryColor: "murple",
+  primaryShade: 5
+});
