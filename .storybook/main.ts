5dada0c853fd7f7e876cc4580cf8dcd1f476bbd2
Pascal Simon <pascaltobiassimon@gmail.com>
2023-12-07 23:57:16 +0100

    Add Mantine shell to Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,14 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: [
-    "../**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
+  stories: ["../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@storybook/addon-styling-webpack",
+    "storybook-dark-mode",
   ],
   framework: {
     name: "@storybook/nextjs",
