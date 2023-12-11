16d206f776bbe4015f93ceba2cb695e99cf0afdb
Pascal Simon <pascaltobiassimon@gmail.com>
2023-12-11 10:17:17 +0100

    Add new colors to theme

--- theme/mantine.ts
+++ theme/mantine.ts
@@ -7,6 +7,8 @@ export const theme = createTheme({
   colors: {
     murple: generateColors("hsl(269, 80%, 56%)"),
     mink: generateColors("rgb(238, 102, 217)"),
+    gold: generateColors("rgb(255,215,0)"),
+    darkgray: generateColors("rgba(99, 99, 99, 1)")
   },
   primaryColor: "murple",
   primaryShade: 5
