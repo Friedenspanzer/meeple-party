71a3d32a1746711b5ce0357145f9bf0e3cb3b165
Pascal Simon <pascaltobiassimon@gmail.com>
2023-12-11 10:17:47 +0100

    Add new story structure to old story

--- components/ShareProfile/ShareProfile.stories.tsx
+++ components/ShareProfile/ShareProfile.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import ShareProfile from "./ShareProfile";
 
 const meta = {
-  title: "Share Profile",
+  title: "Parts/Share Profile",
   component: ShareProfile,
   parameters: {
     layout: "centered",
