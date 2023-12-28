08627ecd8353d9dd51ff51962f02b205958baefb
Pascal Simon <pascaltobiassimon@gmail.com>
2023-12-28 10:26:47 +0100

    Reduce data collected for public profile page

--- app/profile/[profileId]/page.tsx
+++ app/profile/[profileId]/page.tsx
@@ -2,13 +2,7 @@ import PublicUserProfilePage from "@/components/pages/profile/PublicUserProfileP
 import { prisma } from "@/db";
 import { cleanUserDetails } from "@/pages/api/user";
 import { isLoggedIn } from "@/utility/serverSession";
-import {
-  Game,
-  GameCollection as PrismaGameCollection,
-  Relationship,
-  RelationshipType,
-  User,
-} from "@prisma/client";
+import { Relationship, User } from "@prisma/client";
 import { notFound, redirect } from "next/navigation";
 import { Metadata } from "next/types";
 
@@ -47,76 +41,14 @@ export default async function ProfilePage({
   }
 }
 
-function getStatus(gameId: number, myCollection: PrismaGameCollection[]) {
-  const relatedGame = myCollection.find((c) => c.gameId === gameId);
-  if (!relatedGame) {
-    return { own: false, wantToPlay: false, wishlist: false };
-  } else {
-    return {
-      own: relatedGame.own,
-      wantToPlay: relatedGame.wantToPlay,
-      wishlist: relatedGame.wishlist,
-    };
-  }
-}
-
-function getAllFriendIds(user: UserWithRelationships): string[] {
-  const sentFriendships = user.sentRelationships
-    .filter((r) => r.type === RelationshipType.FRIENDSHIP)
-    .map((r) => r.recipientId);
-  const receivedFriendships = user.receivedRelationships
-    .filter((r) => r.type === RelationshipType.FRIENDSHIP)
-    .map((r) => r.senderId);
-  return [...sentFriendships, ...receivedFriendships];
-}
-
-function cleanGame(game: Game) {
-  const { updatedAt, ...cleanGame } = game;
-  return cleanGame;
-}
-
-const getUser = async (id: string, loggedInUserId?: string) => {
-  const extendedUserData = await prisma.user.findUnique({
+const getUser = async (id: string) => {
+  const userData = await prisma.user.findUnique({
     where: { id },
-    include: {
-      receivedRelationships: true,
-      sentRelationships: true,
-      favorites: true,
-      games: {
-        where: {
-          own: true,
-        },
-        include: {
-          game: true,
-        },
-      },
-    },
   });
 
-  if (extendedUserData) {
-    const {
-      receivedRelationships,
-      sentRelationships,
-      favorites,
-      games,
-      ...userData
-    } = extendedUserData;
-
-    const isFriend =
-      loggedInUserId &&
-      getAllFriendIds(extendedUserData).includes(loggedInUserId);
-    const isMe = loggedInUserId && extendedUserData.id === loggedInUserId;
-
-    const clean = isMe || isFriend ? userData : cleanUserDetails(userData);
-
-    return {
-      receivedRelationships,
-      sentRelationships,
-      favorites,
-      games,
-      ...clean,
-    };
+  if (userData) {
+    return cleanUserDetails(userData);
+  } else {
+    return null;
   }
-
-  return null;
 };
