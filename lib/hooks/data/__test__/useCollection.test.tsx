1bb65830edf42dedff6bcbdd72ddf4d14996eff4
Pascal Greilach <pascal@greilach.dev>
2024-02-06 17:07:07 +0100

    Add further tests

--- lib/hooks/data/__test__/useCollection.test.tsx
+++ lib/hooks/data/__test__/useCollection.test.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/lib/util/test";
 import { generateUserProfile, render } from "@/utility/test";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@testing-library/jest-dom";
 import { screen, waitFor } from "@testing-library/react";
 import useCollection from "../useCollection";
 
@@ -63,6 +64,86 @@ describe("useCollection hook", () => {
     );
     const actualCollection = JSON.parse(screen.getByTestId("data").innerHTML);
     expect(actualCollection).toEqual(collection);
+    expect(getCollectionMock.mock.calls[0][0]).toBe(user.id);
+  });
+
+  it("returns isLoading during load", async () => {
+    const queryClient = new QueryClient();
+
+    const user = generateUserProfile();
+
+    const { collection, games } = generateCollection(user.id);
+
+    const getCollectionMock = jest.mocked(getCollection);
+    getCollectionMock.mockResolvedValue(
+      convertCollection(collection, games, user.id)
+    );
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <TestComponent userId={user.id} />
+      </QueryClientProvider>
+    );
+
+    expect(screen.getByTestId("isLoading").innerHTML).toBe("true");
+    expect(screen.getByTestId("data")).toBeEmptyDOMElement();
+  });
+
+  it("returns queried data", async () => {
+    const queryClient = new QueryClient();
+
+    const user = generateUserProfile();
+
+    const { collection: serverCollection, games: serverGames } =
+      generateCollection(user.id);
+    const { collection: cachedCollection, games: cachedGames } =
+      generateCollection(user.id);
+
+    queryClient.setQueryData(["collection", user.id], cachedCollection);
+
+    const getCollectionMock = jest.mocked(getCollection);
+    getCollectionMock.mockResolvedValue(
+      convertCollection(serverCollection, serverGames, user.id)
+    );
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <TestComponent userId={user.id} />
+      </QueryClientProvider>
+    );
+
+    await waitFor(() =>
+      expect(screen.getByTestId("isLoading").innerHTML).toBe("false")
+    );
+    const actualCollection = JSON.parse(screen.getByTestId("data").innerHTML);
+    expect(actualCollection).toEqual(cachedCollection);
+    expect(getCollectionMock.mock.calls.length).toBe(0);
+  });
+
+  it("caches data", async () => {
+    const queryClient = new QueryClient();
+
+    const user = generateUserProfile();
+
+    const { collection, games } = generateCollection(user.id);
+
+    const getCollectionMock = jest.mocked(getCollection);
+    getCollectionMock.mockResolvedValue(
+      convertCollection(collection, games, user.id)
+    );
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <TestComponent userId={user.id} />
+      </QueryClientProvider>
+    );
+
+    await waitFor(() =>
+      expect(screen.getByTestId("isLoading").innerHTML).toBe("false")
+    );
+
+    const cachedCollection = queryClient.getQueryData(["collection", user.id]);
+    expect(cachedCollection).toEqual(collection);
   });
 });
 
