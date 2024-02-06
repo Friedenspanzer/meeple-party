d548231da1f8928525367b5d4c0628e817dc0079
Pascal Greilach <pascal@greilach.dev>
2024-02-06 17:00:51 +0100

    Add implementation

--- lib/hooks/data/useCollection.ts
+++ lib/hooks/data/useCollection.ts
@@ -1,13 +1,40 @@
+import { GameCollectionResult } from "@/app/api/v2/user/[userId]/collection/route";
 import { Result } from "@/hooks/data/types";
+import getCollection from "@/lib/dataAccess/collection";
 import { Collection } from "@/lib/datatypes/client/collection";
 import { UserId } from "@/lib/datatypes/client/userProfile";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+
+const thirtySecondsInMilliseconds = 1000 * 30;
 
 export default function useCollection(userId: UserId): Result<Collection> {
   //TODO implement
+  const queryKey = ["collection", userId];
+  const queryClient = useQueryClient();
+
+  const { isLoading, isError, data } = useQuery({
+    queryKey,
+    queryFn: async () => getCollection(userId).then(convertCollection),
+    refetchOnWindowFocus: true,
+    staleTime: thirtySecondsInMilliseconds,
+  });
   return {
-    data: null,
+    data,
     invalidate: () => {},
-    isError: true,
-    isLoading: false,
+    isError,
+    isLoading,
+  };
+}
+
+function convertCollection(collection: GameCollectionResult): Collection {
+  return {
+    user: collection.userId,
+    own: collection.collection.filter((c) => c.own).map((c) => c.game.id),
+    wishlist: collection.collection
+      .filter((c) => c.wishlist)
+      .map((c) => c.game.id),
+    wantToPlay: collection.collection
+      .filter((c) => c.wantToPlay)
+      .map((c) => c.game.id),
   };
 }
