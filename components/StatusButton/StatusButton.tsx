04375b4f4f15b316b9de5f8103e308ba6d7aaf85
Pascal Greilach <pascal@greilach.dev>
2024-03-25 12:38:04 +0100

    Use new StatusButton component to render old one

--- components/StatusButton/StatusButton.tsx
+++ components/StatusButton/StatusButton.tsx
@@ -1,11 +1,6 @@
 import useCollectionStatus from "@/hooks/api/useCollectionStatus";
-import classNames from "classnames";
+import StatusButtonStatic from "@/lib/components/parts/StatusButton/StatusButton";
 import { useCallback, useMemo } from "react";
-import Spinner from "../Spinner/Spinner";
-import IconCollectionOwn from "../icons/CollectionOwn";
-import IconCollectionWantToPlay from "../icons/CollectionWantToPlay";
-import IconCollectionWishlist from "../icons/CollectionWishlist";
-import styles from "./statusbutton.module.css";
 
 interface StatusButtonProps extends React.HTMLAttributes<HTMLDivElement> {
   gameId: number;
@@ -18,24 +13,6 @@ export default function StatusButton({
   ...props
 }: Readonly<StatusButtonProps>) {
   const { data, isLoading, mutate } = useCollectionStatus(gameId);
-  const icon = useMemo(() => {
-    if (status === "own") {
-      return <IconCollectionOwn />;
-    } else if (status === "wanttoplay") {
-      return <IconCollectionWantToPlay />;
-    } else if (status === "wishlist") {
-      return <IconCollectionWishlist />;
-    }
-  }, [status]);
-  const activeColor = useMemo(() => {
-    if (status === "own") {
-      return "var(--color-collection-own)";
-    } else if (status === "wanttoplay") {
-      return "var(--color-collection-want-to-play)";
-    } else if (status === "wishlist") {
-      return "var(--color-collection-wishlist)";
-    }
-  }, [status]);
   const active = useMemo(() => {
     if (!data) {
       return false;
@@ -59,33 +36,13 @@ export default function StatusButton({
     }
   }, [status, mutate, data]);
 
-  if (isLoading) {
-    return (
-      <Spinner
-        {...props}
-        className={classNames(styles.spinner, props.className)}
-      />
-    );
-  } else {
-    return (
-      <div
-        {...props}
-        onClick={updateStatus}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            updateStatus();
-          }
-        }}
-        tabIndex={0}
-        className={classNames(styles.button, props.className)}
-        style={{
-          ...props.style,
-          color: active ? activeColor : "unset",
-        }}
-        role="button"
-      >
-        {icon}
-      </div>
-    );
-  }
+  return (
+    <StatusButtonStatic
+      active={active || false}
+      loading={isLoading}
+      status={status}
+      clicked={updateStatus}
+      {...props}
+    />
+  );
 }
