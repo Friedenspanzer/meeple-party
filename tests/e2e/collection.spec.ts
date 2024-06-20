4450cca19f56e447639bfda727a5d4338c99db69
Pascal Greilach <pascal@greilach.dev>
2024-06-20 19:40:10 +0200

    Add tests for game collection pagination

---
+++ tests/e2e/collection.spec.ts
@@ -0,0 +1,82 @@
+import { expect, test } from "@playwright/test";
+import { User } from "@prisma/client";
+import { AVAILABLE_GAMES } from "./globalSetup";
+import {
+  addGamesToCollection,
+  clearCollection,
+  deleteUser,
+  logInAsNewUser,
+} from "./utility";
+
+let user: User | undefined;
+
+test.describe("Pagination", () => {
+  test.beforeEach(async ({ page }) => {
+    user = await logInAsNewUser(page.context());
+    await clearCollection(user.id);
+    await addGamesToCollection(user.id, AVAILABLE_GAMES);
+  });
+  test.afterEach(async () => {
+    if (user) {
+      deleteUser(user.id);
+    }
+  });
+  test("has expected pages with small gamebox", async ({ page }) => {
+    await page.goto("/app/collection");
+    await page.getByRole("radiogroup").locator("label").first().click();
+    await expect(page.getByRole("button", { name: "1" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "2" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "3" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "4" })).toBeVisible();
+    await expect(page.getByLabel("Current page")).toHaveValue("1");
+    await expect(page.getByText("of 4")).toBeVisible();
+    await expect(page.getByRole("button", { name: "1" })).toHaveClass(/active/);
+  });
+  test("has expected pages with big gamebox", async ({ page }) => {
+    await page.goto("/app/collection");
+    await page.getByRole("radiogroup").locator("label").nth(1).click();
+    await expect(page.getByRole("button", { name: "1" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "2" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "3" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "6" })).toBeVisible();
+    await expect(page.getByText("…6")).toBeVisible();
+    await expect(page.getByLabel("Current page")).toHaveValue("1");
+    await expect(page.getByText("of 6")).toBeVisible();
+    await expect(page.getByRole("button", { name: "1" })).toHaveClass(/active/);
+  });
+  test("button display changes with changing pages", async ({ page }) => {
+    await page.goto("/app/collection");
+    await page.getByRole("radiogroup").locator("label").nth(1).click();
+    await page.getByRole("button", { name: "3" }).click();
+    await expect(page.getByRole("button", { name: "3" })).toHaveClass(/active/);
+    await expect(page.getByRole("button", { name: "4" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "5" })).toBeVisible();
+    await expect(page.getByLabel("Current page")).toHaveValue("3");
+  });
+  test("button changes pages", async ({ page }) => {
+    await page.goto("/app/collection");
+    await expect(
+      page.getByRole("heading", { name: "7 Wonders Duel" })
+    ).toBeVisible();
+    await expect(page.getByRole("heading", { name: "Ark Nova" })).toBeVisible();
+    await expect(
+      page.getByRole("heading", { name: "Brass: Lancashire" })
+    ).toBeVisible();
+    await page.getByRole("button", { name: "3" }).click();
+    await expect(
+      page.getByRole("heading", { name: "Frosthaven" })
+    ).toBeVisible();
+    await expect(
+      page.getByRole("heading", { name: "Lost Ruins of Arnak" })
+    ).toBeVisible();
+    await page.getByRole("button", { name: "6" }).click();
+    await expect(
+      page.getByRole("heading", { name: "The Crew: Mission Deep Sea" })
+    ).toBeVisible();
+    await expect(page.getByRole("heading", { name: "Wingspan" })).toBeVisible();
+  });
+  test("inputting pages changes current page",async ({page}) => {
+    await page.goto("/app/collection");
+    await page.getByLabel('Current page')
+  })
+});
--- tests/e2e/globalSetup.ts
+++ tests/e2e/globalSetup.ts
@@ -1,9 +1,25 @@
 import { prisma } from "@/db";
+import { getGameData } from "@/utility/games";
 import { generateString } from "@/utility/test";
 import { FullConfig, chromium } from "@playwright/test";
 import { ADMIN, ADMIN_STORAGE, USER, USER_STORAGE } from "./constants";
 
+export const AVAILABLE_GAMES = [
+  224517, 161936, 174430, 342942, 233078, 316554, 167791, 115746, 187645,
+  291457, 162886, 220308, 12333, 182028, 193738, 84876, 169786, 246900, 173346,
+  28720, 167355, 266507, 177736, 124361, 205637, 266192, 341169, 120677, 312484,
+  295770, 237182, 164928, 192135, 96848, 199792, 251247, 324856, 183394, 175914,
+  366013, 285774, 256960, 247763, 3076, 295947, 521, 102794, 284378, 185343,
+  170216, 184267, 314040, 31260, 255984, 221107, 161533, 205059, 253344, 126163,
+  276025,
+];
+
 export default async function globalSetup(config: FullConfig) {
+  await createUsers();
+  await createGames();
+}
+
+async function createUsers() {
   const adminUser = await prisma.user.create({
     data: { ...ADMIN, role: "ADMIN" },
   });
@@ -52,4 +68,8 @@ export default async function globalSetup(config: FullConfig) {
   await userContext.storageState({ path: USER_STORAGE });
 
   await browser.close();
+}
+
+async function createGames() {
+  await getGameData(AVAILABLE_GAMES);
 }
