18e2420aa14f8b675d10cffc66596ee72638d742
Pascal Greilach <pascal@greilach.dev>
2024-06-21 07:38:03 +0200

    Optimize Playwright execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,8 +13,8 @@ export default defineConfig({
   testDir: "./tests/e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: !!process.env.CI ? 1 : 0,
+  workers: "50%",
   reporter: "html",
   use: {
     baseURL: "http://127.0.0.1:3000",
