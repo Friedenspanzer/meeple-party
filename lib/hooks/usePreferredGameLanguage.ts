ea7204c55d9f1354cdf168ca7369ebf3a37571a2
Pascal Greilach <pascal@greilach.dev>
2024-07-05 11:59:59 +0200

    Add tests an documentation to hook

--- lib/hooks/usePreferredGameLanguage.ts
+++ lib/hooks/usePreferredGameLanguage.ts
@@ -1,9 +1,19 @@
 import { useUserPreferences } from "@/hooks/useUserPreferences";
 import { determineGameLanguage } from "@/i18n/lib";
 import { fallbackLng } from "@/i18n/settings";
+import { GameLanguage } from "@/i18n/types";
 
-//TODO Test, document
-export default function usePreferredGameLanguage() {
+interface UsePreferredGameLanguageResult {
+  /** Wether results are still loading. */
+  loading: boolean;
+  /** Language the user wants to see game names in. */
+  preferredGameLanguage: GameLanguage;
+}
+
+/**
+ * Use the language the user preferes to see game names in. Defaults to the fallback language during load.
+ */
+export default function usePreferredGameLanguage(): UsePreferredGameLanguageResult {
   const { loading, preferences } = useUserPreferences();
 
   if (loading) {
