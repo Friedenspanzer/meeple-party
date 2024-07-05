8e79b11965ca42a9926cd0670460f12ddb6a8568
Pascal Greilach <pascal@greilach.dev>
2024-07-05 10:53:54 +0200

    Add E2E tests (#143)
    * Install Playwright
    
    * Add simple test
    
    * Move Playwright tests to more specific folder
    
    * Add simple tests
    
    * Fix pagination
    
    * Add tests for game collection pagination
    
    * Use simplified login for older tests
    
    * Add build step to workflow
    
    * Consolidate into single workflow
    
    * Add node module cache to all jobs
    
    * Add database to test runner
    
    * Add database build jobs
    
    * Fix CLI calls
    
    * Do not test mobile devices
    
    * Update e2e job to cache browser binaries
    
    * Update caching strategy for workflow
    
    * Optimize Playwright execution
    
    * Ensure feature flags are there
    
    * Rename workflow to reflect what it actually does
    
    * Fix Playwright version not available in cache check
    
    * Add environment variable to suppress warning
    
    * Reset workers to default for enhanced stability
    
    * Hopefully fix Playwright binaries caching
    
    * Add publishing Playwright report to GitHub pages
    
    * Run publish step on test failure
    
    * Fix expression syntax
    
    * Fix version check for Playwright binary cache
    
    * Make cache handling explicit
    
    This should cache results even on job failure
    
    * Fix cache update not triggering
    
    * Split browser and dependency installation
    
    * Remove browser binary caching
    
    That saved only a few seconds and was very prone to errors.
    
    * Streamline node_modules caching
    
    * Use default npm caching instead of custom built cache
    
    * Fix tests not working in Webkit
    
    As Webkit uses the advanced tracking protection it's very picky about cookie urls and I set mine wrong!
    
    * Add visual test to front page
    
    * Add video generation to config
    
    * Add tests for sharing my profile
    
    * Add error logging to game fetching function
    
    Hopefully this will help me pin down why the e2e tests often fail in CI.
    
    * Add script for running e2e tests
    
    * Wait for user session check to terminate before doing visual test
    
    * Remove example tests
    
    * Remove snapshots from repository
    
    I do understand now why people don't just commit them to the repository! n_n
    
    * Add environment variables to update snapshot step
    
    * Add Next.js build cache to CI
    
    * Do snapshot cache handling after database setup
    
    * Add ignore patterns to code coverage
    
    * Split initial game fetching into smaller chunks
    
    BGG seems to reject some queries when the response would be too long.

---
+++ tests/e2e/globalTeardown.ts
@@ -0,0 +1,3 @@
+import { FullConfig } from "@playwright/test";
+
+export default async function globalTeardown(config: FullConfig) {}
---
+++ tests/e2e/utility.ts
@@ -0,0 +1,82 @@
+import { GameId } from "@/datatypes/game";
+import { prisma } from "@/db";
+import { generateString } from "@/utility/test";
+import { faker } from "@faker-js/faker";
+import { BrowserContext } from "@playwright/test";
+import { Role, User } from "@prisma/client";
+
+export async function addGamesToCollection(userId: string, gameIds: GameId[]) {
+  const user = await prisma.user.findUnique({ where: { id: userId } });
+  if (!user) {
+    throw Error("User not found");
+  }
+  await prisma.gameCollection.createMany({
+    data: gameIds.map((gameId) => ({
+      gameId,
+      own: true,
+      userId: user.id,
+      wantToPlay: false,
+      wishlist: false,
+    })),
+  });
+}
+
+export async function removeGamesFromCollection(
+  userId: string,
+  gameIds: GameId[]
+) {
+  const user = await prisma.user.findUnique({ where: { id: userId } });
+  if (!user) {
+    throw Error("User not found");
+  }
+  await prisma.gameCollection.deleteMany({
+    where: { userId: user.id, gameId: { in: gameIds } },
+  });
+}
+
+export async function clearCollection(userId: string) {
+  const user = await prisma.user.findUnique({ where: { id: userId } });
+  if (!user) {
+    throw Error("User not found");
+  }
+  await prisma.gameCollection.deleteMany({
+    where: { userId: user.id },
+  });
+}
+
+export async function logInAsNewUser(
+  context: BrowserContext,
+  role: Role = Role.USER
+): Promise<User> {
+  const user = await prisma.user.create({
+    data: {
+      email: faker.internet.email(),
+      emailVerified: new Date(),
+      name: faker.internet.displayName(),
+      place: faker.location.city(),
+      realName: faker.person.fullName(),
+      profileComplete: true,
+      role,
+    },
+  });
+  const session = await prisma.session.create({
+    data: {
+      userId: user.id,
+      sessionToken: generateString(),
+      expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+    },
+  });
+  context.addCookies([
+    {
+      name: "next-auth.session-token",
+      value: session.sessionToken,
+      url: "http://127.0.0.1:3000",
+      httpOnly: true,
+    },
+  ]);
+  return user;
+}
+
+export async function deleteUser(id: string) {
+  await prisma.user.delete({ where: { id } });
+}
