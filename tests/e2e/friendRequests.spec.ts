f412de1218c64b0020dc158f76b85a474e5d2885
Pascal Greilach <pascal@greilach.dev>
2024-10-14 10:38:51 +0200

    Add page synchronization E2E tests

--- tests/e2e/friendRequests.spec.ts
+++ tests/e2e/friendRequests.spec.ts
@@ -67,6 +67,42 @@ test.describe("Friend requests on profile page", () => {
       page.getByRole("button", { name: "Send friend request" })
     ).toBeVisible();
   });
+  test.describe("changes immediately visible in friend request overview", () => {
+    test("sent request", async ({ page }) => {
+      const myself = await logInAsNewUser(page.context());
+      const otherUser = await createUser();
+
+      page.goto(`/app/profile/${otherUser.id}`);
+      await page.getByRole("button", { name: "Send friend request" }).click();
+
+      page.goto("/app/friends/requests");
+      await expect(page.getByText(otherUser.name || "")).toBeVisible();
+      await expect(
+        page.getByRole("button", { name: "Withdraw your friend request" })
+      ).toBeVisible();
+    });
+    test("withdrawn friend request", async ({ page }) => {
+      const myself = await logInAsNewUser(page.context());
+      const otherUser = await createUser();
+
+      await sendFriendRequest(myself.id, otherUser.id);
+
+      page.goto("/app/friends/requests");
+      await expect(page.getByText(otherUser.name || "")).not.toBeVisible();
+    });
+    test("accepted friend request", async ({ page }) => {
+      const myself = await logInAsNewUser(page.context());
+      const otherUser = await createUser();
+
+      await sendFriendRequest(otherUser.id, myself.id);
+
+      page.goto(`/app/profile/${otherUser.id}`);
+      await page.getByRole("button", { name: "Accept" }).click();
+
+      page.goto("/app/friends/requests");
+      await expect(page.getByText(otherUser.name || "")).not.toBeVisible();
+    });
+  });
 });
 
 test.describe("Friend requests on overview page", () => {
@@ -101,5 +137,38 @@ test.describe("Friend requests on overview page", () => {
     page.goto("/app/friends/requests");
     await page.getByRole("button", { name: "Deny" }).click();
     await expect(page.getByText(otherUser.name || "")).not.toBeVisible();
+  });
+
+  test.describe("changes immediately visible on profile page", () => {
+    test("withdrawn friend request", async ({ page }) => {
+      const myself = await logInAsNewUser(page.context());
+      const otherUser = await createUser();
+
+      await sendFriendRequest(myself.id, otherUser.id);
+
+      page.goto("/app/friends/requests");
+      await page
+        .getByRole("button", { name: "Withdraw your friend request" })
+        .click();
+
+      page.goto(`/app/profile/${otherUser.id}`);
+      await expect(
+        page.getByRole("button", { name: "Send friend request" })
+      ).toBeVisible();
+    });
+    test("accepted friend request", async ({ page }) => {
+      const myself = await logInAsNewUser(page.context());
+      const otherUser = await createUser();
+
+      await sendFriendRequest(otherUser.id, myself.id);
+
+      page.goto("/app/friends/requests");
+      await page.getByRole("button", { name: "Accept" }).click();
+
+      page.goto(`/app/profile/${otherUser.id}`);
+      await expect(
+        page.getByRole("button", { name: "End friendship" })
+      ).toBeVisible();
+    });
   });
 });
