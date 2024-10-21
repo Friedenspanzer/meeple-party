26269478452130c242a8cef3b99126ca4bd44ce9
Pascal Greilach <pascal@greilach.dev>
2024-10-21 07:28:31 +0200

    Move Footer component to the only page where it is used

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import Analytics from "@/components/Analytics/Analytics";
 import AuthProvider from "@/context/authContext";
 import UserProvider from "@/context/userContext";
-import Footer from "@/lib/components/structures/Footer/Footer";
+import Footer from "@/app/_components/Footer/Footer";
 import "@/theme/theme.scss";
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
